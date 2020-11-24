b0c730075aa3bca226ed266c74dfea92a2fb4076
cydaw6 <cydaw6@gmail.com>
2020-11-24 12:28:31 +0100

    save to prevent fail from upgrade to java 14

--- src/main/java/dut/info/console/Event.java
+++ src/main/java/dut/info/console/Event.java
@@ -9,10 +9,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Event {
@@ -22,8 +19,8 @@ public class Event {
 	private final String title;
 	private ArrayList<Action> actions;
 
-	private Event(int id, Season season, String title, ArrayList<Action> actions) {
-		this.id = id;
+	private Event(Season season, String title, ArrayList<Action> actions) {
+		this.id = GameUtils.idByHashString(title);
 		this.title = Objects.requireNonNull(title, "Event's title can't be null");
 		this.season = season;
 		this.actions = actions;
@@ -62,21 +59,52 @@ public class Event {
 				return (i > 0) && name.substring(i + 1).equals("json");
 			}).collect(Collectors.toList()));
 		}
-		// List of all event from parsing the events json files
-		List<Event> eventsList = alleventsFiles.stream().map(x -> {
-			//System.out.println(x.getPath());
-			Object jsonEvents =  GameUtils.jsonToObject(x.getPath());
-			JSONArray events = (JSONArray) jsonEvents;
-			Object o1 = events.get(0);
-			JSONObject event = (JSONObject) o1;
-			int id = (int) (long) event.get("id");
-			String title = (String) event.get("title");
-			Object factionsO = event.get("factions");
-			
+		// List of all events from parsing all the json files
+		List<Event> eventsList = new ArrayList<>();
+
+		HashMap<String, List<String>> failedFiles;
+		for(File f: alleventsFiles) {
+			// Global array of the file
+			JSONArray ar = (JSONArray) ((Object) GameUtils.jsonToObject(f.getPath()));
+			ar.stream().forEach(x -> {
+				/* Parse the file x */
+				String title; List<Action> actions; Season season; //init Event() parameters
+				JSONObject eventO = (JSONObject) x;
+				season = switch ((int) eventO.get("season")){
+					case Season.Spring:
+						return Season.Spring;
+
+
+
+				};
+				title = (String) eventO.get("title");
+				//actions
+				String actionTitle;
+				JSONArray factionsO = (JSONArray) eventO.get("actions");
+				JSONObject o2 = (JSONObject) ((JSONArray) eventO.get("actions")).get(0);
+				actionTitle = (String) o2.get("title");
+
+
+
+
+
+
+				eventsList.add(new Event());
+			});
+
+			;
+		}
+
+/**
+ *
+
+
+
 
 			return new Event();
 		}).collect(Collectors.toList());
 		//alleventsFiles.stream().forEach(x -> System.out.println(x.getParentFile().getName()));
+ */
 		return eventsList;
 	}
 
